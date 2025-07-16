4be93835b69fd88fff47d5048ec7bac2e3e38dca
Anhuar Ruiz <myclxss@outlook.com>
2025-07-16 15:15:37 -0600

    added: managers and task classes, update settings

---
+++ src/main/java/dev/anhuar/antiDebuff/manager/AntiDebuffManager.java
@@ -0,0 +1,31 @@
+package dev.anhuar.antiDebuff.manager;
+
+/*
+ * ========================================================
+ * AntiDebuff - AntiDebuffManager.java
+ *
+ * @author Anhuar Ruiz | Anhuar Dev | myclass
+ * @web https://anhuar.dev
+ * @date 15/07/25
+ *
+ * License: MIT License - See LICENSE file for details.
+ * Copyright (c) 2025 Anhuar Dev. All rights reserved.
+ * ========================================================
+ */
+
+import dev.anhuar.antiDebuff.AntiDebuff;
+
+public class AntiDebuffManager {
+
+    private final AntiDebuff plugin = AntiDebuff.getInstance();
+
+    public void add() {
+
+    }
+
+    public void remove() {
+
+    }
+
+
+}
---
+++ src/main/java/dev/anhuar/antiDebuff/task/EffectTask.java
@@ -0,0 +1,18 @@
+package dev.anhuar.antiDebuff.task;
+
+/*
+ * ========================================================
+ * AntiDebuff - EffectTask.java
+ *
+ * @author Anhuar Ruiz | Anhuar Dev | myclass
+ * @web https://anhuar.dev
+ * @date 16/07/25
+ *
+ * License: MIT License - See LICENSE file for details.
+ * Copyright (c) 2025 Anhuar Dev. All rights reserved.
+ * ========================================================
+ */
+
+public class EffectTask {
+
+}
